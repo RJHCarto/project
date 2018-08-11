4de8526384651b5cae1730343c62c4ac99184424
RJHCarto <33897218+RJHCarto@users.noreply.github.com>
2018-08-11 15:38:28 +0200

    Test version 0.1

---
+++ MapBox/MapBoxStandAlone/js/fitMenu.js
@@ -0,0 +1,76 @@
+/**
+ * Return the geoJSON object for the selected layer.
+ * @param layer
+ * @returns {Promise<any>}
+ */
+
+function getLayerGeoJSON(layer) {
+    console.log(" -- getLayerGeoJSON --")
+    return new Promise((resolve, reject) => {
+        coordsList = [];
+        $.getJSON(baseURL.concat(window[layer]), function (data) {
+            resolve(data)
+        });
+    });
+}
+
+function bboxFromJSON(json) {
+    return new Promise((resolve, reject) => {
+        resolve(turf.bbox(json));
+    });
+}
+
+function fit(bbox) {
+    let bboxToUse = bbox || SELECTED_LAYER_BBOX;
+    map.fitBounds(bboxToUse, {padding: 20});
+    mapB.fitBounds(bboxToUse, {padding: 20});
+}
+
+function switchLayer(layer) {
+    console.dir(layer)
+    SELECTED_LAYER_ID = layer;
+    //layerId = layer.target.id;
+    // set layer id to a menu value
+    // If id = UCL
+    if (layer == 'UCL') {
+        map.setLayoutProperty('EatonBray', 'visibility', 'none');
+        map.setLayoutProperty('Wavendon', 'visibility', 'none');
+        map.setLayoutProperty('UCL', 'visibility', 'visible');
+        mapB.setLayoutProperty('EatonBray', 'visibility', 'none');
+        mapB.setLayoutProperty('Wavendon', 'visibility', 'none');
+        mapB.setLayoutProperty('UCL', 'visibility', 'visible');
+    }
+    // If id = Wavendon
+    if (layer == 'Wavendon') {
+        map.setLayoutProperty('EatonBray', 'visibility', 'none');
+        map.setLayoutProperty('Wavendon', 'visibility', 'visible');
+        map.setLayoutProperty('UCL', 'visibility', 'none');
+        mapB.setLayoutProperty('EatonBray', 'visibility', 'none');
+        mapB.setLayoutProperty('Wavendon', 'visibility', 'visible');
+        mapB.setLayoutProperty('UCL', 'visibility', 'none');
+    }
+    // Else id = EatonBray
+    if (layer == 'EatonBray') {
+        map.setLayoutProperty('EatonBray', 'visibility', 'visible');
+        map.setLayoutProperty('Wavendon', 'visibility', 'none');
+        map.setLayoutProperty('UCL', 'visibility', 'none');
+        mapB.setLayoutProperty('EatonBray', 'visibility', 'visible');
+        mapB.setLayoutProperty('Wavendon', 'visibility', 'none');
+        mapB.setLayoutProperty('UCL', 'visibility', 'none');
+    }
+    getLayerGeoJSON(layer).then((json) => {
+        console.dir(json)
+        SELECTED_LAYER_JSON = json;
+        console.log("SELECTED LAYER COORDS CHANGED")
+        bboxFromJSON(json).then((bbox) => {
+            SELECTED_LAYER_BBOX = bbox;
+            fit(bbox)
+        });
+    });
+}
+
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].onclick = (e) => {
+        switchLayer(e.target.id)
+    };
+}
---
+++ MapBox/MapBoxStandAlone/js/intersect.js
@@ -0,0 +1,171 @@
+function getIntersect() {
+    var interArray = [];
+    var interDist = [];
+    var sumWidths = [];
+    var avgArray = [];
+    var avgArrayPoints = [];
+    var buildingPoints = [];
+    var buildingFeatures = [];
+    var selectedPoints;
+    var uniqueBuildings;
+    var buildingHeights = [];
+    var selectedBuildings;
+    var heightGraph;
+
+    for (var i = 0; i < mapB.getStyle().layers.length; i++) {
+        if ( mapB.getStyle().layers[i].id == 'HighlightedBuildings' ) {
+            // mapB.removeLayer('IntersectPoints');
+            // mapB.removeSource('IntersectPoints');
+            mapB.removeLayer('HighlightedBuildings');
+            mapB.removeSource('HighlightedBuildings');
+            map.removeLayer('HighlightedBuildings');
+            map.removeSource('HighlightedBuildings');
+            break
+        }
+    }
+
+    console.log(" --Performing Intersect-- ")
+    var drawnLine = draw.getAll();
+    var layerFeatures = turf.featureCollection(SELECTED_LAYER_JSON.features);
+    intersectingFeatures = turf.lineIntersect(drawnLine, layerFeatures);
+
+    console.log(" --Making Intersect Array-- ")
+    for (var i = 0; i < intersectingFeatures.features.length; i++) {
+        interArray.push(intersectingFeatures.features[i].geometry.coordinates)
+    }
+    interArray = interArray.sort();
+
+    console.log("--Making point distance array--");
+    for (var i = 0; i < interArray.length-1; i++) {
+        pt1 = turf.point(interArray[i]);
+        pt2 = turf.point(interArray[i+1]);
+        interDist.push((turf.distance(pt1, pt2, {unit: 'kilometers'})));
+
+    }
+    console.dir(interDist);
+
+    console.log("--Making cumulative point distance array--");
+    sumWidths.push((interDist[0]/2));
+    for (var i = 1; i < interDist.length; i++) {
+        first = math.sum(interDist.slice(0,i));
+        second = (interDist[i]/2);
+        sumWidths.push(first+second);
+    }
+    console.dir(sumWidths);
+
+    console.log("--Making midpoint array--")
+    for (var i = 0; i < (interArray.length-1); i++) {
+        avgCoords = [];
+        avgCoord0 = ((interArray[i][0]+interArray[i+1][0])/2);
+        avgCoord1= ((interArray[i][1]+interArray[i+1][1])/2);
+        avgCoords.push(avgCoord0, avgCoord1)
+        avgArray.push(avgCoords)
+    }
+
+    console.log("--Making midpoint points--")
+    avgArray.forEach(item => {
+        point = turf.point(item);
+        avgArrayPoints.push(point);
+    });
+
+
+    for (var i = 0; i < avgArrayPoints.length; i++) {
+        height = 0;
+        for (var j = 0; j < SELECTED_LAYER_JSON.features.length ; j++) {
+            var status = turf.booleanPointInPolygon(avgArrayPoints[i],SELECTED_LAYER_JSON.features[j]);
+            if (status == true) {
+                buildingFeatures.push(SELECTED_LAYER_JSON.features[j]);
+                height = SELECTED_LAYER_JSON.features[j].properties.relh2;
+            }
+        }
+        buildingHeights.push(height);
+    }
+
+    console.log(buildingHeights)
+
+    uniqueBuildings = _.uniq(buildingFeatures);
+
+    selectedBuildings = turf.featureCollection(uniqueBuildings);
+
+    // for (var i = 0; i < selectedBuildings.features.length; i++) {
+    //     buildingHeights.push((selectedBuildings.features[i].properties.relh2).toFixed(1));
+    // }
+
+    chart = document.getElementById('chart');
+
+
+    var heightGraph = {
+        type: 'bar',
+        x: sumWidths,
+        y: buildingHeights,
+        width: interDist,
+        marker: {
+            color: '#5CA8A6'
+        }
+    }
+
+    var data = [heightGraph];
+
+    var layout = {
+        title: 'Building Heights',
+        xaxis: {title: 'Segment Distance'},
+        yaxis:{title:'Height (relh2)'},
+        bargap:0
+    };
+
+    Plotly.plot(chart, data, layout);
+
+
+    console.log("--Highlighting Buildings--")
+
+    mapB.addSource('HighlightedBuildings', {
+        type: 'geojson',
+        data: selectedBuildings
+    });
+
+    mapB.addLayer({
+        id: 'HighlightedBuildings',
+        source: 'HighlightedBuildings',
+        type: 'fill',
+        paint: {
+            'fill-color': '#263746',
+            'fill-opacity': 0.6
+        },
+    });
+
+    map.addSource('HighlightedBuildings', {
+        type: 'geojson',
+        data: selectedBuildings
+    });
+
+    map.addLayer({
+        id: 'HighlightedBuildings',
+        source: 'HighlightedBuildings',
+        type: 'fill-extrusion',
+        paint: {
+            'fill-extrusion-color': '#263746',
+            'fill-extrusion-height': {
+                'type': 'identity',
+                'property': 'relh2'
+            },
+            'fill-extrusion-base': 0,
+            'fill-extrusion-opacity': 0.6
+        }
+    });
+
+
+}
+
+function removeIntersect() {
+    draw.deleteAll(draw.getAll());
+    for (var i = 0; i < mapB.getStyle().layers.length; i++) {
+        if (mapB.getStyle().layers[i].id == 'HighlightedBuildings') {
+            mapB.removeLayer('HighlightedBuildings');
+            map.removeLayer('HighlightedBuildings');
+            mapB.removeSource('HighlightedBuildings');
+            map.removeSource('HighlightedBuildings');
+            break
+        }
+    }
+    Plotly.deleteTraces(chart,0);
+}
